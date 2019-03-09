cfcaa83f35222bcca3a658f3e5e761bf4da58009
Mathieu PANTCHENKO <mathieu.pantchenko@edu.univ-fcomte.fr>
2019-03-09 12:19:16 +0100

    inchallahaussi

--- db/migrate/20190301094919_create_students.rb
+++ db/migrate/20190301094919_create_students.rb
@@ -1,8 +1,8 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.string :name
-      t.date :birthday
+      t.date :birthdate
 
       t.timestamps
     end
