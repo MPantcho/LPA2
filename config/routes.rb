54b26eaeb2de39cd3fd1ba6290b381a607475789
Mathieu PANTCHENKO <mathieu.pantchenko@edu.univ-fcomte.fr>
2019-03-29 20:00:01 +0100

    modele3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :produits
-  resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
