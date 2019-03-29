389730c71964c78c38e22aea837fbabd5e2ec883
Mathieu PANTCHENKO <mathieu.pantchenko@edu.univ-fcomte.fr>
2019-03-29 20:24:18 +0100

    modele4

--- db/migrate/20190322094709_create_orderlines.rb
+++ db/migrate/20190322094709_create_orderlines.rb
@@ -2,7 +2,7 @@ class CreateOrderlines < ActiveRecord::Migration[5.2]
   def change
     create_table :orderlines do |t|
       t.references :order, foreign_key: true
-      t.references :produits, foreign_key: true
+      t.references :produit, foreign_key: true
 
       t.timestamps
     end
